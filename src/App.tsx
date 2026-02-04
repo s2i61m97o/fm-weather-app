3539ae1408c54d7470a0194975df54ccae3e6876
matthewsimpson <simpson.matthew.j97@gmail.com>
2026-02-04 09:50:35 +0000

    section(currentForecast): implement section
    - create location/time and current weather section
    - use placeholder information

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./styles/App.css";
 import Header from "./sections/Header/Header";
 import Search from "./sections/Search/Search";
+import CurrentForecast from "./sections/CurrentForecast/CurrentForcast";
 
 function App() {
   return (
     <main>
       <Header />
       <Search />
+      <CurrentForecast />
     </main>
   );
 }
