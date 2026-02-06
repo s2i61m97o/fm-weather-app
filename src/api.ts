322b1a38f196b64d6f84d449a80633b83dfbdd7f
matthewsimpson <simpson.matthew.j97@gmail.com>
2026-02-06 09:23:07 +0000

    refactor(getQueryLocations): use URLSearchParams
    - use URLSearchParams constructor
    - create const for Geocoding API URL

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,15 @@
+const GEOCODEURL = "https://geocoding-api.open-meteo.com/v1/search?";
+
 export const getQueryLocations = async (input: string) => {
+  const searchParams = new URLSearchParams();
+  searchParams.set("name", input);
+  searchParams.set("count", "10");
+  searchParams.set("language", "en");
+  searchParams.set("format", "json");
+  const searchURL = GEOCODEURL + searchParams.toString();
+
   try {
-    const res = await fetch(
-      `https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(
-        input,
-      )}&count=10&language=en&format=json`,
-    );
+    const res = await fetch(searchURL);
     if (!res.ok) {
       throw new Error("Error Response");
     }
