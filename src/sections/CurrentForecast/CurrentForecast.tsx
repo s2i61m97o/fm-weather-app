bde838bdebef740fad24edf8529263283ceb7bde
matthewsimpson <simpson.matthew.j97@gmail.com>
2026-02-08 10:48:51 +0000

    feat(CurrentForecast): data placeholders
    - dynamic date

--- src/sections/CurrentForecast/CurrentForecast.tsx
+++ src/sections/CurrentForecast/CurrentForecast.tsx
@@ -11,19 +11,32 @@ export default function CurrentForecast({
   forecast,
   locationName,
 }: CurrentForecastProps) {
-  if (!forecast) {
-    return;
-  }
-
   const iconSrc = getIcon(forecast?.weather_code);
+
+  const date = new Date().toLocaleDateString("utc", {
+    weekday: "long",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+
   const cardData = [
     {
       title: "feels like",
-      data: `${Math.round(forecast?.apparent_temperature)}˚`,
+      data: forecast ? `${Math.round(forecast?.apparent_temperature)}˚` : "-",
     },
-    {title: "humidity", data: `${forecast?.relative_humidity_2m}%`},
-    {title: "wind", data: `${Math.round(forecast?.wind_speed_10m)} km/h`},
-    {title: "precipitation", data: `${Math.round(forecast?.precipitation)} mm`},
+    {
+      title: "humidity",
+      data: forecast ? `${forecast?.relative_humidity_2m}%` : "-",
+    },
+    {
+      title: "wind",
+      data: forecast ? `${Math.round(forecast?.wind_speed_10m)} km/h` : "-",
+    },
+    {
+      title: "precipitation",
+      data: forecast ? `${Math.round(forecast?.precipitation)} mm` : "-",
+    },
   ];
 
   const cards = cardData.map((data) => {
@@ -38,18 +51,18 @@ export default function CurrentForecast({
   return (
     <section className={styles.forecast}>
       <div className={styles.forecast__weatherInfo}>
-        {forecast && (
-          <>
-            <div className={styles.forecast__locationInfo}>
-              <h2>{locationName}</h2>
-              <p>Monday, Jan 1, 2026</p>
-            </div>
-            <div className={styles.forecast__temp}>
-              <img src={iconSrc} alt="" className={styles.forecast__icon} />
-              <p>{forecast ? Math.round(forecast.temperature_2m) : "-"}˚</p>
-            </div>
-          </>
-        )}
+        <div className={styles.forecast__locationInfo}>
+          <h2>{locationName}</h2>
+          <p>{date}</p>
+        </div>
+        <div className={styles.forecast__temp}>
+          {forecast ? (
+            <img src={iconSrc} alt="" className={styles.forecast__icon} />
+          ) : (
+            <div className={styles.forecast__icon}></div>
+          )}
+          <p>{forecast ? Math.round(forecast.temperature_2m) : "-"}˚</p>
+        </div>
       </div>
       <div className={styles.forecast__container}>{cards}</div>
     </section>
